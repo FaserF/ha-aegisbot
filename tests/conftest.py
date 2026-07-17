ac8c87fcf5da70a83b777bdeffdeda4f3ce282b5
Fabian Seitz <fabian.seitz@pari.com>
2026-07-17 18:50:08 +0200

    fix CI tests & linter

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 def auto_enable_custom_integrations(hass):
     """Enable custom integrations in Home Assistant."""
     hass.data.pop("custom_components", None)
+    hass.config.components.add("hassio")
     yield
 
 
